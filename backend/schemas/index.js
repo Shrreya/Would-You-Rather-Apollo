7766a400b16cbad3ae0a304db5e020a897cca7dc
Erik Claesson <kirekire95@gmail.com>
2020-01-30 17:06:30 +0100

    Added dotenv, prettier and eslint, as well as modified the files to be more readable to me

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,32 +1,40 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express")
 
 const typeDefs = gql`
-    type User {
-        _id: ID,
-        userName: String!,
-        firstName: String!,
-        lastName: String!
-    },
-    type Question {
-        _id: ID,
-        author: String!,
-        timestamp: String!,
-        optionOne: Option,
-        optionTwo: Option
-    },
-    type Option {
-        votes: [String],
-        text: String!
-    }
-    type Query {
-        users: [User],
-        questions: [Question]
-    },
-    type Mutation {
-        addUser(userName: String!, firstName: String!, lastName: String!): User,
-        addQuestion(author: String!, optionOneText: String!, optionTwoText: String!): Question,
-        saveAnswer(questionId: String!, userName: String!, option: String!): Question
-    }
+  type User {
+    _id: ID
+    userName: String!
+    firstName: String!
+    lastName: String!
+  }
+  type Question {
+    _id: ID
+    author: String!
+    timestamp: String!
+    optionOne: Option
+    optionTwo: Option
+  }
+  type Option {
+    votes: [String]
+    text: String!
+  }
+  type Query {
+    users: [User]
+    questions: [Question]
+  }
+  type Mutation {
+    addUser(userName: String!, firstName: String!, lastName: String!): User
+    addQuestion(
+      author: String!
+      optionOneText: String!
+      optionTwoText: String!
+    ): Question
+    saveAnswer(
+      questionId: String!
+      userName: String!
+      option: String!
+    ): Question
+  }
 `
 
 module.exports = typeDefs
